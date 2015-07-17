679fc5b4d7c9128a4e16df457c6b1a2980071025
Prateek Agarwal <prat0318@gmail.com>
2015-07-17 02:44:09 -0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-apt-get update && apt-get install -y zsh openssh-client git curl vim python-dev python-pip wget
+# curl www.prat0318.me/init.sh | zsh
+apt-get install -y zsh
 echo "Adding new user prat0318..."
 # adduser --ingroup sudo --shell /usr/bin/zsh --disabled-login --gecos "" prat0318
 useradd -m -g sudo -s /usr/bin/zsh -p prat0318 prat0318
