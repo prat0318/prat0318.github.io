135c6586e7ee392572a82bda657473a3fa41f2c9
Prateek Agarwal <prat0318@gmail.com>
2015-07-18 08:59:41 -0700

    Update create.sh

--- create.sh
+++ create.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# curl www.prat0318.me/create.sh -o /tmp/create.sh; zsh /tmp/create.sh
+# source < (curl -s www.prat0318.me/create.sh)
 sudo apt-get update && apt-get install -y openssh-client git curl vim python-dev python-pip wget
 mkdir -p ~/.ssh
 ssh-keygen -t rsa -b 4096 -C "prat0318@gmail.com" -N "" -f ~/.ssh/id_rsa
